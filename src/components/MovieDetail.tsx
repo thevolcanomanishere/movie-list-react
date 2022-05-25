90ae316a34d568d2861f58d24a3bbb7c4b92abcb
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-05-25 15:09:38 +0100

    Add movie detail + synopsis toggle

--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -1,7 +1,6 @@
-import { Fragment, useState } from 'react'
-import { Dialog, RadioGroup, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useState } from 'react'
+import { Dialog, Switch, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
-import { StarIcon } from '@heroicons/react/solid'
 import { MovieItem } from '../types'
 
 const product = {
@@ -41,10 +40,44 @@ interface MovieDetailProps {
 
 export default function MovieDetail(props: MovieDetailProps) {
   const { isOpenParent, movieItem, setClosedParent } = props;
-
   const [movie] = useState<MovieItem>(movieItem);
-  const [selectedColor, setSelectedColor] = useState(product.colors[0]);
-  const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
+  const [showFullSynopsis, setShowFullSynopsis] = useState(false);
+  const [toggle, setToggle] = useState(false);
+
+  const renderSynopsosis = () => {
+    if(showFullSynopsis){
+      return (
+        <p className="text-sm text-gray-900" dangerouslySetInnerHTML={{__html: movie.synopsis}}/>
+      )
+    }
+    return (
+      <p className="pt-10 text-2xl text-gray-900">{movie.synopsisShort}</p>
+    )
+  }
+
+  const renderToggle = () => {
+    return (
+      <div>
+        <Switch
+          checked={toggle}
+          onChange={setToggle}
+          className={`${toggle ? 'bg-teal-900' : 'bg-teal-700'}
+            relative inline-flex h-[20px] w-[40px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+        >
+          <span className="sr-only">Use setting</span>
+          <span
+            aria-hidden="true"
+            className={`${toggle ? 'translate-x-5' : 'translate-x-0'}
+              pointer-events-none inline-block h-[15px] w-[15px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+          />
+        </Switch>
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    setShowFullSynopsis(toggle);
+  }, [toggle])
 
   return (
     <Transition.Root show={isOpenParent} as={Fragment}>
@@ -85,153 +118,22 @@ export default function MovieDetail(props: MovieDetailProps) {
 
                   <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
                     <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
-                      <img src={product.imageSrc} alt={product.imageAlt} className="object-center object-cover" />
+                      <img src={movie.image} alt={'Movie poster'} className="object-center object-cover" />
                     </div>
                     <div className="sm:col-span-8 lg:col-span-7">
                       <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">{movie.name}</h2>
-
-                      <section aria-labelledby="information-heading" className="mt-2">
-                        <h3 id="information-heading" className="sr-only">
-                          Product information
-                        </h3>
-
-                        <p className="text-2xl text-gray-900">{product.price}</p>
-
-                        {/* Reviews */}
-                        <div className="mt-6">
-                          <h4 className="sr-only">Reviews</h4>
-                          <div className="flex items-center">
-                            <div className="flex items-center">
-                              {[0, 1, 2, 3, 4].map((rating) => (
-                                <StarIcon
-                                  key={rating}
-                                  className={classNames(
-                                    product.rating > rating ? 'text-gray-900' : 'text-gray-200',
-                                    'h-5 w-5 flex-shrink-0'
-                                  )}
-                                  aria-hidden="true"
-                                />
-                              ))}
-                            </div>
-                            <p className="sr-only">{product.rating} out of 5 stars</p>
-                            <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                              {product.reviewCount} reviews
-                            </a>
-                          </div>
-                        </div>
-                      </section>
-
+                      <p className="text-2xl text-black">Released: {movie.productionYear}</p>
+                      
+                      <div className='flex'>
+                        <p className="text-xl text-gray-900">Synopsis:</p>
+                        {renderToggle()}
+                      </div>
+                      {renderSynopsosis()}
                       <section aria-labelledby="options-heading" className="mt-10">
                         <h3 id="options-heading" className="sr-only">
                           Product options
                         </h3>
 
-                        <form>
-                          {/* Colors */}
-                          <div>
-                            <h4 className="text-sm text-gray-900 font-medium">Color</h4>
-
-                            <RadioGroup value={selectedColor} onChange={setSelectedColor} className="mt-4">
-                              <RadioGroup.Label className="sr-only">Choose a color</RadioGroup.Label>
-                              <span className="flex items-center space-x-3">
-                                {product.colors.map((color) => (
-                                  <RadioGroup.Option
-                                    key={color.name}
-                                    value={color}
-                                    className={({ active, checked }) =>
-                                      classNames(
-                                        color.selectedClass,
-                                        active && checked ? 'ring ring-offset-1' : '',
-                                        !active && checked ? 'ring-2' : '',
-                                        '-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none'
-                                      )
-                                    }
-                                  >
-                                    <RadioGroup.Label as="span" className="sr-only">
-                                      {color.name}
-                                    </RadioGroup.Label>
-                                    <span
-                                      aria-hidden="true"
-                                      className={classNames(
-                                        color.class,
-                                        'h-8 w-8 border border-black border-opacity-10 rounded-full'
-                                      )}
-                                    />
-                                  </RadioGroup.Option>
-                                ))}
-                              </span>
-                            </RadioGroup>
-                          </div>
-
-                          {/* Sizes */}
-                          <div className="mt-10">
-                            <div className="flex items-center justify-between">
-                              <h4 className="text-sm text-gray-900 font-medium">Size</h4>
-                              <a href="#" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                                Size guide
-                              </a>
-                            </div>
-
-                            <RadioGroup value={selectedSize} onChange={setSelectedSize} className="mt-4">
-                              <RadioGroup.Label className="sr-only">Choose a size</RadioGroup.Label>
-                              <div className="grid grid-cols-4 gap-4">
-                                {product.sizes.map((size) => (
-                                  <RadioGroup.Option
-                                    key={size.name}
-                                    value={size}
-                                    disabled={!size.inStock}
-                                    className={({ active }) =>
-                                      classNames(
-                                        size.inStock
-                                          ? 'bg-white shadow-sm text-gray-900 cursor-pointer'
-                                          : 'bg-gray-50 text-gray-200 cursor-not-allowed',
-                                        active ? 'ring-2 ring-indigo-500' : '',
-                                        'group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1'
-                                      )
-                                    }
-                                  >
-                                    {({ active, checked }) => (
-                                      <>
-                                        <RadioGroup.Label as="span">{size.name}</RadioGroup.Label>
-                                        {size.inStock ? (
-                                          <span
-                                            className={classNames(
-                                              active ? 'border' : 'border-2',
-                                              checked ? 'border-indigo-500' : 'border-transparent',
-                                              'absolute -inset-px rounded-md pointer-events-none'
-                                            )}
-                                            aria-hidden="true"
-                                          />
-                                        ) : (
-                                          <span
-                                            aria-hidden="true"
-                                            className="absolute -inset-px rounded-md border-2 border-gray-200 pointer-events-none"
-                                          >
-                                            <svg
-                                              className="absolute inset-0 w-full h-full text-gray-200 stroke-2"
-                                              viewBox="0 0 100 100"
-                                              preserveAspectRatio="none"
-                                              stroke="currentColor"
-                                            >
-                                              <line x1={0} y1={100} x2={100} y2={0} vectorEffect="non-scaling-stroke" />
-                                            </svg>
-                                          </span>
-                                        )}
-                                      </>
-                                    )}
-                                  </RadioGroup.Option>
-                                ))}
-                              </div>
-                            </RadioGroup>
-                          </div>
-
-                          <button
-                            type="submit"
-                            className="mt-6 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                          >
-                            Add to bag
-                          </button>
-                        </form>
                       </section>
                     </div>
                   </div>
