b4d46e2e232c02a605abab45e075ef15f502b97a
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-05-25 15:09:56 +0100

    Remove unused code

--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -3,35 +3,6 @@ import { Dialog, Switch, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
 import { MovieItem } from '../types'
 
-const product = {
-  name: 'Basic Tee 6-Pack ',
-  price: '$192',
-  rating: 3.9,
-  reviewCount: 117,
-  href: '#',
-  imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg',
-  imageAlt: 'Two each of gray, white, and black shirts arranged on table.',
-  colors: [
-    { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
-    { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
-    { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
-  ],
-  sizes: [
-    { name: 'XXS', inStock: true },
-    { name: 'XS', inStock: true },
-    { name: 'S', inStock: true },
-    { name: 'M', inStock: true },
-    { name: 'L', inStock: true },
-    { name: 'XL', inStock: true },
-    { name: 'XXL', inStock: true },
-    { name: 'XXXL', inStock: false },
-  ],
-}
-
-function classNames(...classes : any) {
-  return classes.filter(Boolean).join(' ')
-}
-
 interface MovieDetailProps {
   movieItem: MovieItem;
   isOpenParent: boolean;
